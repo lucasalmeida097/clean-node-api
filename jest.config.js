811f2872b5d531d21e222446b5bcd1f63242eeba
Lucas <lucasalmeida_097@outlook.com>
2024-01-21 06:30:57 -0300

    add jest

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+
+const config = {
+  collectCoverage: true,
+
+  coverageDirectory: 'coverage',
+
+  coverageProvider: 'v8'
+
+}
+
+module.exports = config
