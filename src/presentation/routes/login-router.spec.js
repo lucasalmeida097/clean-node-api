cff2c4daada1be7f676bb355d97ca795ddc5d88d
Lucas <lucasalmeida_097@outlook.com>
2024-01-23 21:48:24 -0300

    remove duplicated code

--- src/presentation/routes/login-router.spec.js
+++ src/presentation/routes/login-router.spec.js
@@ -1,6 +1,7 @@
 class LoginRouter {
-  route (httpResponse) {
-    if (!httpResponse.body.email || !httpResponse.body.password) {
+  route (httpRequest) {
+    const { email, password } = httpRequest.body
+    if (!email || !password) {
       return {
         statusCode: 400
       }
